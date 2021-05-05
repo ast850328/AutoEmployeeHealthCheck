b4707195df86f15c551e6d2e2ca4eadeadb77f10
SafeFly <ast850328@gmail.com>
2021-05-06 00:20:09 +0800

    fix: update crawler due to form changing

--- src/crawler.ts
+++ src/crawler.ts
@@ -17,25 +17,21 @@ async function crawlWeb(url: string, workerNumber: string) {
   await page.goto(url);
 
   // agree button
-  await page.click('input[id="486014833_3209788694"]');
+  await page.click('input[id="647719974_4255613818"]');
   // worker number
-  await page.type('input[id="486014830"]', workerNumber);
-  // temperature measurement method
-  await page.click('input[id="486014835_3209788699"]');
-  // random temperature 36 ~ 38
-  const temperature = _getRandom().toString();
-  await page.type('input[id="486014831"]', temperature);
+  await page.type('input[id="647719971"]', workerNumber);
+  // temperature
+  await page.click('input[id="647719976_4255613822"]');
   // close contacted people
-  await page.click('input[id="486015076_3209796414"]');
+  await page.click('input[id="647719981_4255613851"]');
   // declaration
-  await page.click('input[id="486014832_3209788684"]');
+  await page.click('input[id="647719973_4255613808"]');
 
   await page.click('button[type=submit]');
 
   await page.waitForNavigation();
 
   logger.info('Worker Number: ', workerNumber);
-  logger.info('Temperature : ', temperature);
 
   let isSucceed = false;
   if (page.url().includes('HCCompleted')) {
@@ -47,8 +43,8 @@ async function crawlWeb(url: string, workerNumber: string) {
     logger.info(`Failed worker number: ${workerNumber}`);
   }
 
-  sendBotMessage(workerNumber, temperature, isSucceed);
-  
+  sendBotMessage(workerNumber, isSucceed);
+
   await browser.close();
 }
 
