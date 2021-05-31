76f4d1dbed29f8268d178d16f227718c57a59690
SafeFly <ast850328@gmail.com>
2021-05-31 09:23:24 +0800

    fix: update form 5/26

--- src/crawler.ts
+++ src/crawler.ts
@@ -17,22 +17,24 @@ async function crawlWeb(url: string, workerNumber: string) {
   await page.goto(url);
 
   // agree button
-  await page.click('input[id="659398437_4331991557"]');
+  await page.click('input[id="660768081_4340945613"]');
   // worker number
-  await page.type('input[id="659398434"]', workerNumber);
+  await page.type('input[id="660768078"]', workerNumber);
   // temperature method
-  await page.click('input[id="659398439_4331991560"]');
+  await page.click('input[id="660768083_4340945616"]');
   // temperature
   const temperature = _getRandom().toString();
-  await page.type('input[id="659398435"]', temperature);
+  await page.type('input[id="660768079"]', temperature);
   // close contacted people
-  await page.click('input[id="659398444_4331991584"]');
+  await page.click('input[id="660768088_4340945640"]');
   // broadcast messaging
-  await page.click('input[id="659398445_4331991586"]');
+  await page.click('input[id="660768089_4340945642"]');
   // accepted the PCR nucleic acid test or got the positive of COVID-19 rapid test
-  await page.click('input[id="659398446_4331991588"]');
+  await page.click('input[id="660768090_4340945644"]');
+  // stayed in any indoor place for more than 2 hours in Taipei city or New Taipei city in the past 14 days?
+  await page.click('input[id="660769069_4340992401"]');
   // declaration
-  await page.click('input[id="659398436_4331991549"]');
+  await page.click('input[id="660768080_4340945605"]');
 
   await page.click('button[type=submit]');
 
