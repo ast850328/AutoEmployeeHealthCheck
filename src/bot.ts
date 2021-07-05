069011668b9d430ccc8a26fb0d12f100510cd649
SafeFly <ast850328@gmail.com>
2021-07-05 09:07:29 +0800

    chore: prettier

--- src/bot.ts
+++ src/bot.ts
@@ -1,15 +1,21 @@
-import * as dotenv from 'dotenv';
-import TelegramBot, * as telegramBot from 'node-telegram-bot-api';
+import * as dotenv from "dotenv";
+import TelegramBot, * as telegramBot from "node-telegram-bot-api";
 
 dotenv.config();
-const botToken: string = process.env.BOT_TOKEN ? process.env.BOT_TOKEN : '';
-const channelId: string = process.env.CHANNEL_ID ? process.env.CHANNEL_ID : '';
+const botToken: string = process.env.BOT_TOKEN ? process.env.BOT_TOKEN : "";
+const channelId: string = process.env.CHANNEL_ID ? process.env.CHANNEL_ID : "";
 
-const bot = new TelegramBot(botToken, {polling: true});
+const bot = new TelegramBot(botToken, { polling: true });
 
-function sendBotMessage(workerNumber: string, temperature: string, isSucceed: boolean) {
-    const message: string = `${workerNumber} ${temperature} ${isSucceed ? '✅' : '❌'}`;
-    bot.sendMessage(channelId, message, { parse_mode: 'Markdown' });
+function sendBotMessage(
+  workerNumber: string,
+  temperature: string,
+  isSucceed: boolean
+) {
+  const message: string = `${workerNumber} ${temperature} ${
+    isSucceed ? "✅" : "❌"
+  }`;
+  bot.sendMessage(channelId, message, { parse_mode: "Markdown" });
 }
 
 export { sendBotMessage };
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,40 @@
-import { CronJob } from 'cron';
-import * as log4js from 'log4js';
-import * as dotenv from 'dotenv';
+import { CronJob } from "cron";
+import * as log4js from "log4js";
+import * as dotenv from "dotenv";
 
-import { crawlWeb } from './crawler';
+import { crawlWeb } from "./crawler";
 
 log4js.configure({
-  appenders: { crawler: { type: 'file', filename: 'crawler.log' } },
-  categories: { default: { appenders: ['crawler'], level: 'info' } },
+  appenders: { crawler: { type: "file", filename: "crawler.log" } },
+  categories: { default: { appenders: ["crawler"], level: "info" } },
 });
-const logger = log4js.getLogger('crawler');
+const logger = log4js.getLogger("crawler");
 
 dotenv.config();
-const url: string = process.env.URL ? process.env.URL : '';
-const cronTime: string = process.env.CRON_TIME ? process.env.CRON_TIME : '* * * * * *';
-const workerNumbers: Array<string> = process.env.WORKER_NUMBERS ? process.env.WORKER_NUMBERS?.split(',') : [''];
+const url: string = process.env.URL ? process.env.URL : "";
+const cronTime: string = process.env.CRON_TIME
+  ? process.env.CRON_TIME
+  : "* * * * * *";
+const workerNumbers: Array<string> = process.env.WORKER_NUMBERS
+  ? process.env.WORKER_NUMBERS?.split(",")
+  : [""];
 
-const job = new CronJob(cronTime, () => {
-  logger.info('start job');
-  workerNumbers.forEach((workerNumber) => {
-    crawlWeb(url, workerNumber)
-    .catch(error => {
-      logger.error(error);
-      logger.error(`Failed worker number: ${workerNumber}`);
-    });;
-  });
-}, null, true, 'Asia/Taipei');
+const job = new CronJob(
+  cronTime,
+  () => {
+    logger.info("start job");
+    workerNumbers.forEach((workerNumber) => {
+      crawlWeb(url, workerNumber).catch((error) => {
+        logger.error(error);
+        logger.error(`Failed worker number: ${workerNumber}`);
+      });
+    });
+  },
+  null,
+  true,
+  "Asia/Taipei"
+);
 
 job.start();
-logger.info('AutoEmployeeHealthCheck is on.');
+logger.info("AutoEmployeeHealthCheck is on.");
 logger.info(`The worker numbers are : ${workerNumbers}`);
