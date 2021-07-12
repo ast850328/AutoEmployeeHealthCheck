5b4d7841c443d25fc14a0784570e2b21a875acdb
SafeFly <ast850328@gmail.com>
2021-07-12 10:53:42 +0800

    fix: update form for 7/9 version

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,8 +1,6 @@
 import * as log4js from "log4js";
 import puppeteer from "puppeteer";
 
-import { sendBotMessage } from "./bot";
-
 const logger = log4js.getLogger("crawler");
 
 function _getRandom(): number {
@@ -18,57 +16,42 @@ async function crawlWeb(url: string, workerNumber: string) {
   const page = await browser.newPage();
   await page.goto(url);
 
-  // first page
   // agree button
-  await page.click('input[id="677104488_4450212741"]');
+  await page.click('input[id="679468817_4466173239"]');
   // worker number
-  await page.type('input[id="677104485"]', workerNumber);
+  await page.type('input[id="679468814"]', workerNumber);
   // temperature method
-  await page.click('input[id="677104490_4450212744"]');
+  await page.click('input[id="679468819_4466173242"]');
   // temperature
   const temperature = _getRandom().toString();
-  await page.type('input[id="677104486"]', temperature);
+  await page.type('input[id="679468815"]', temperature);
   // high risk person
-  await page.click('input[id="677104495_4450212768"]');
+  await page.click('input[id="679468824_4466173266"]');
   // symptoms of COVID-19 in the past 7 days
-  await page.click('input[id="677104496_4450212769"]');
+  await page.click('input[id="679468825_4466173267"]');
   // overlap with the confirmed person’s footprint
-  await page.click('input[id="677104701_4450214118"]');
-  // high risk hot spot
-  await page.click('input[id="677104498_4450212787"]');
+  await page.click('input[id="679468831_4466173346"]');
+  // accepted the PCR nucleic acid test
+  await page.click('input[id="679468826_4466173270"]');
+  // last rapid test result in 7 days
+  await page.click('input[id="679468830_4466173337"]');
+  // declaration
+  await page.click('input[id="679468816_4466173231"]');
   // next page
   await page.click("button[type=submit]");
-
-  await page.waitForNavigation();
-
-  // second page
-  // accepted the PCR nucleic acid test
-  await page.click('input[id="677104497_4450212772"]');
-  // last rapid test result in 7 days
-  await page.click('input[id="677104501_4450212839"]');
-  // declaration
-  await page.click('input[id="677104487_4450212733"]');
-  // next page
-  await page.click(
-    "button.btn.small.next-button.survey-page-button.user-generated.notranslate"
-  );
 
   await page.waitForNavigation();
 
   logger.info("Worker Number: ", workerNumber);
   logger.info("Temperature: ", temperature);
 
-  let isSucceed = false;
-  if (page.url().includes("HCCompleted")) {
-    logger.info(`Completed worker number: ${workerNumber}`);
-    isSucceed = true;
-  } else {
-    logger.info(`Failed worker number: ${workerNumber}`);
-  }
-
-  sendBotMessage(workerNumber, temperature, isSucceed);
-
+  const completedUrl: string = page.url();
   await browser.close();
+
+  if (!completedUrl.includes("HCCompleted")) {
+    Promise.reject();
+  }
+  Promise.resolve();
 }
 
 export { crawlWeb };
